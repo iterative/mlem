61e0ea727a85b9ebdb0ef38839af11e143678dbd
Alexander Guschin <1aguschin@gmail.com>
2021-11-09 14:35:46 +0300

    format with black

--- mlem/api/__init__.py
+++ mlem/api/__init__.py
@@ -4,4 +4,14 @@ MLEM's Python API
 from ..core.metadata import load, load_meta, save
 from .commands import apply, get, init, link, ls, pack
 
-__all__ = ["save", "load", "load_meta", "ls", "get", "init", "link", "pack", "apply"]
+__all__ = [
+    "save",
+    "load",
+    "load_meta",
+    "ls",
+    "get",
+    "init",
+    "link",
+    "pack",
+    "apply",
+]
