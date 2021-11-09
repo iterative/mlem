33194e7a3ec10e2c205143133232d38612dda9b1
Mikhail Sveshnikov <mike0sv@gmail.com>
2021-11-09 12:45:08 +0300

    fix empty config exception (#105)
    * fix empty config exception
    
    Signed-off-by: mike0sv <mike0sv@gmail.com>
    
    * debug print
    
    * revert debug print
    
    * debug print
    
    * debug print
    
    * fix s3 deps?
    
    * bump s3 deps?
    
    * revert debug print
    
    * s3fs?
    
    * revert boto pin
    
    * revert revert boto pin
    
    * revert revert boto pin

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ lightgbm = ["lightgbm"]
 
 # serve & deploy
 fastapi = ["uvicorn", "fastapi"]
-sagemaker = ["boto3", "sagemaker"]
+sagemaker = ["boto3==1.19.12", "sagemaker"]
 
 all_libs = (
     dvc
@@ -110,7 +110,9 @@ tests = [
     # we use this to suppress some messages in tests, eg: foo/bar naming,
     # and, protected method calls in our tests
     "pylint-plugin-utils",
-    "s3fs",
+    "s3fs==2021.10.1",
+    "boto3==1.19.12",
+    "botocore==1.22.12",
     "adlfs",
     "gcsfs",
 ] + all_libs
