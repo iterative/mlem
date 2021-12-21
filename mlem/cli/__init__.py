021f8407809a9b971d2fc5b63c66dd679fc327e1
mike0sv <mike0sv@gmail.com>
2021-12-21 23:51:48 +0300

    clone rename

--- mlem/cli/__init__.py
+++ mlem/cli/__init__.py
@@ -2,10 +2,10 @@
 MLEM's command-line interface
 """
 from mlem.cli.apply import apply
+from mlem.cli.clone import clone
 from mlem.cli.config import config
 from mlem.cli.deploy import deploy
 from mlem.cli.env import environment
-from mlem.cli.get import clone
 from mlem.cli.info import ls, pretty_print
 from mlem.cli.init import init
 from mlem.cli.link import link
--- mlem/cli/clone.py
+++ mlem/cli/clone.py
@@ -2,7 +2,14 @@ from typing import Optional
 
 import click
 
-from mlem.cli.main import mlem_command, option_repo, option_rev
+from mlem.cli.main import (
+    mlem_command,
+    option_external,
+    option_link,
+    option_repo,
+    option_rev,
+    option_target_repo,
+)
 
 
 @mlem_command("clone")
@@ -10,9 +17,28 @@ from mlem.cli.main import mlem_command, option_repo, option_rev
 @click.option("-t", "--target", help="Path to store the downloaded object.")
 @option_repo
 @option_rev
-def clone(uri: str, target: str, repo: Optional[str], rev: Optional[str]):
+@option_target_repo
+@option_link
+@option_external
+def clone(
+    uri: str,
+    target: str,
+    repo: Optional[str],
+    rev: Optional[str],
+    target_repo: Optional[str],
+    external: Optional[bool],
+    link: Optional[bool],
+):
     """Download MLEM object from {uri} and save it to {out}."""
     from mlem.api.commands import clone
 
     click.echo(f"Downloading {uri} to {target}")
-    clone(uri, target, repo=repo, rev=rev, target_repo=target_repo, external=external, link=link)
+    clone(
+        uri,
+        target,
+        repo=repo,
+        rev=rev,
+        target_repo=target_repo,
+        external=external,
+        link=link,
+    )
