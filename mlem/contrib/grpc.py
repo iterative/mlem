84a1043ddc70c2ccd14053362b9eb515dd6364a6
Madhur Tandon <madhur16053@iiitd.ac.in>
2022-02-07 12:25:07 +0530

    create messages from generic

--- mlem/contrib/grpc.py
+++ mlem/contrib/grpc.py
@@ -1,7 +1,8 @@
-from typing import Dict, List, Optional
+from typing import Dict, List, Optional, Type
 from pydantic import BaseModel, ConstrainedList
 from mlem.core.model import Signature
 from mlem.runtime import Interface
+import typing_inspect
 import inspect
 
 class GRPCField(BaseModel):
@@ -52,6 +53,28 @@ def create_message_from_constraint_list(type_, list_models=None, suffix=None, me
     return [message] + list_models
 
 
+def create_message_from_generic(type_, message_name, generic_models=None):
+    if generic_models is None:
+        generic_models = []
+    generic_type = type_.__origin__
+    inner_type = type_.__args__[0]
+
+    if generic_type is list:
+        field_rule = "repeated"
+        suffix = "_list"
+    
+    if typing_inspect.is_generic_type(inner_type):
+        message_name+=suffix
+        field = GRPCField(rule=field_rule, type_=message_name, key="_", id_=1)
+        generic_models.extend(create_message_from_generic(inner_type, message_name, generic_models))
+        message_name = message_name[:-len(suffix)]
+    else:
+        field = GRPCField(rule=field_rule, type_=inner_type.__name__, key="_", id_=1)
+    
+    message = GRPCMessage(name=message_name, fields=[field])
+    return [message] + generic_models
+
+
 def get_models_recursively(model: BaseModel, field_models=None):
     if field_models is None:
         field_models = []
@@ -62,21 +85,24 @@ def get_models_recursively(model: BaseModel, field_models=None):
         field_rule = None
         outer_type_ = each_field_val.outer_type_
         type_ = each_field_val.type_
-        if issubclass(type_, ConstrainedList):
+        # if typing_inspect.is_generic_type(outer_type_):
+        #     generic_models = create_message_from_generic(outer_type_, message_name=each_field_val.name)
+        #     field_models.extend(generic_models)
+        if inspect.isclass(type_) and issubclass(type_, ConstrainedList):
             list_models = create_message_from_constraint_list(type_)
             field_models.extend(list_models)
         if inspect.isclass(type_) and issubclass(type_, BaseModel):
             field_models.extend(get_models_recursively(type_, field_models))
-        if outer_type_ != type_:
-            collection_type = outer_type_._name
-            if collection_type == "List":
-                field_rule = "repeated"
+        field_rule = "repeated" if outer_type_ != type_ and outer_type_._name == "List" else None
         field_type = type_.__name__
         num_fields+=1
         field = GRPCField(rule=field_rule, type_=field_type, key=each_field_key, id_=num_fields)
         all_fields.append(field)
-    message = GRPCMessage(name=message_name, fields=all_fields)
-    return [message] + field_models
+    if all_fields:
+        message = GRPCMessage(name=message_name, fields=all_fields)
+        return [message] + field_models
+    else:
+        return field_models
 
 
 def create_messages(signature: Signature):
@@ -117,11 +143,49 @@ if __name__ == '__main__':
     import pandas as pd
     from mlem.core.objects import ModelMeta
     from mlem.runtime.interface.base import ModelInterface
+    from mlem.core.dataset_type import DatasetType, DatasetWriter, DatasetSerializer
+    from mlem.core.requirements import Requirements
+    from typing import Any, ClassVar
 
-    train, target = load_iris(return_X_y=True)
+    class Container(DatasetType, DatasetSerializer):
+        __name__ = "container"
+        type: ClassVar[str] = "test_container"
+        field: List[List[List[int]]]
+
+        def serialize(self, instance: Any) -> dict:
+            return instance
+
+        def deserialize(self, obj: dict) -> Any:
+            return obj
+
+        def get_requirements(self) -> Requirements:
+            return Requirements.new([])
+
+        def get_writer(self, **kwargs) -> DatasetWriter:
+            raise NotImplementedError()
+        
+        def get_serializer(
+            self, **kwargs  # pylint: disable=unused-argument
+        ) -> "DatasetSerializer":
+            if isinstance(self, DatasetSerializer):
+                return self
+            raise NotImplementedError
+
+        def get_model(self) -> Type[BaseModel]:
+            return self
+
+    train, target = load_iris(return_X_y=True, as_frame=True)
     train = pd.DataFrame(train)
     model = DecisionTreeClassifier().fit(train, target)
     interface = ModelInterface.from_model(ModelMeta.from_obj(model, sample_data=train))
-    rpc_definitions, rpc_messages = create_grpc_protobuf(interface)
-    print(rpc_definitions)
-    print(list(set(rpc_messages)))
+
+    dt = interface.get_method_returns("predict_proba")
+    # dt = NumpyNdarrayType(shape=(3,3,3), dtype="float64")
+    # dt  = interface.get_method_args("predict")['data']
+    # dt = Container(field=[[[3,3,3]]])
+
+    print("\n".join(list(set(create_messages(Signature(name="aaa", args=[], returns=dt))))))
+    # print(dt.get_serializer().get_model().schema())
+    # rpc_definitions, rpc_messages = create_grpc_protobuf(interface)
+    # print(rpc_definitions)
+    # print(list(set(rpc_messages)))
