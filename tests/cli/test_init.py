7a7c84fd2709d2bf814d0729f5038d3f9e85e236
mike0sv <mike0sv@gmail.com>
2022-04-04 19:16:09 +0300

    fix tests

--- tests/cli/test_init.py
+++ tests/cli/test_init.py
@@ -7,7 +7,7 @@ from tests.cli.conftest import Runner
 
 
 def test_init(runner: Runner, tmpdir):
-    result = runner.invoke(f"init {make_posix(tmpdir)}")
+    result = runner.invoke(f"init {make_posix(str(tmpdir))}")
     assert result.exit_code == 0, result.exception
     assert os.path.isdir(tmpdir / MLEM_DIR)
     assert os.path.isfile(tmpdir / MLEM_DIR / CONFIG_FILE_NAME)
