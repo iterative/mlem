bd0cd8e8b40d188925bd1ea650b21e8610a4c951
mike0sv <mike0sv@gmail.com>
2022-05-17 15:03:07 +0300

    and another one closes #250

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 install_requires = [
     "dill",
     "requests",
-    "isort>=5",
+    "isort>=5.10",
     "docker",
     "pydantic>=1.9.0,<2",
     "typer",
