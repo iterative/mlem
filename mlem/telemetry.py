4e7f706fccca39c8b27acae6655cf80ccd8fdc6e
mike0sv <mike0sv@gmail.com>
2022-05-30 16:54:56 +0300

    change url and fix test

--- mlem/telemetry.py
+++ mlem/telemetry.py
@@ -4,5 +4,8 @@ from mlem import CONFIG
 from mlem.version import __version__
 
 telemetry = IterativeTelemetryLogger(
-    "mlem", __version__, not CONFIG.NO_ANALYTICS, debug=True
+    "mlem",
+    __version__,
+    not CONFIG.TESTS and not CONFIG.NO_ANALYTICS,
+    url="https://telemetry.mlem.dev/api/v1/s2s/event?ip_policy=strict",
 )
