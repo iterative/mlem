275c65646c114191c6ca5d1d28fe4eaa02b146b0
mike0sv <mike0sv@gmail.com>
2022-06-21 20:40:17 +0300

    fix double serialization

--- mlem/core/data_type.py
+++ mlem/core/data_type.py
@@ -198,9 +198,7 @@ class PrimitiveType(DataType, DataHook, DataSerializer):
         return Requirements.new()
 
     def get_model(self, prefix: str = "") -> Type[BaseModel]:
-        return create_model(
-            prefix + self.ptype.capitalize(), __root__=(self.to_type, ...)
-        )
+        return self.to_type
 
 
 class PrimitiveWriter(DataWriter):
