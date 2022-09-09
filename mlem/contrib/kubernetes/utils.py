ef2bacec33d07aa613e2b21e62c14c2f10a15b5d
Madhur Tandon <madhurtandon23@gmail.com>
2022-09-10 05:27:22 +0530

    hot swapping of docker image deployed

--- mlem/contrib/kubernetes/utils.py
+++ mlem/contrib/kubernetes/utils.py
@@ -1,15 +1,48 @@
+import json
 import os
 import tempfile
 
 from kubernetes import client, utils, watch
 
+from .context import K8sYamlGenerator
 
-def create_k8s_resources(generator):
+
+def create_k8s_resources(generator: K8sYamlGenerator):
     k8s_client = client.ApiClient()
     with tempfile.TemporaryDirectory(prefix="mlem_k8s_yaml_build_") as tempdir:
         filename = os.path.join(tempdir, "resource.yaml")
         generator.write(filename)
-        utils.create_from_yaml(k8s_client, filename, verbose=True)
+        try:
+            utils.create_from_yaml(k8s_client, filename, verbose=True)
+        except utils.FailToCreateError as e:
+            failures = e.api_exceptions
+            for each_failure in failures:
+                error_info = json.loads(each_failure.body)
+                if error_info["reason"] != "AlreadyExists":
+                    raise e
+                else:
+                    if error_info["details"]["kind"] == "deployments":
+                        api_instance = client.AppsV1Api()
+                        body = {
+                            "spec": {
+                                "template": {
+                                    "spec": {
+                                        "containers": [
+                                            {
+                                                "name": f"{generator.image_name}",
+                                                "image": f"{generator.image_uri}",
+                                            }
+                                        ]
+                                    }
+                                }
+                            }
+                        }
+                        api_instance.patch_namespaced_deployment(
+                            generator.image_name,
+                            generator.namespace,
+                            body,
+                            pretty=True,
+                        )
 
 
 def pod_is_running(namespace, timeout=60) -> bool:
