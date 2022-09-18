022201e4e045cdb627dc885c9c0e6e28bdf1ed10
Anderson Singh <ttandersonsingh@gmail.com>
2022-09-18 01:21:01 +0000

    added test for stderr_echo context manager to fix  warning from codecov

--- tests/cli/test_stderr.py
+++ tests/cli/test_stderr.py
@@ -1,6 +1,8 @@
+from io import StringIO
 from unittest import mock
 
 from mlem.core.errors import MlemError
+from mlem.ui import echo, stderr_echo
 
 EXCEPTION_MESSAGE = "Test Exception Message"
 
@@ -40,3 +42,16 @@ def test_stderr_mlem_error(runner):
         )
         assert len(result.stderr) > 0, "Output is empty, but should not be"
         assert MLEM_ERROR_MESSAGE in result.stderr
+
+
+STDERR_MESSAGE = "Test Stderr Message"
+
+
+def test_stderr_echo():
+    with mock.patch("sys.stderr", new_callable=StringIO) as mock_stderr:
+        with stderr_echo():
+            echo(STDERR_MESSAGE)
+            mock_stderr.seek(0)
+            output = mock_stderr.read()
+            assert len(output) > 0, "Output is empty, but should not be"
+            assert STDERR_MESSAGE in output
