0e14152baeec198525f1463a748d009d1726f728
Anderson Singh <ttandersonsingh@gmail.com>
2022-09-20 16:35:28 +0000

    added tests for verbose and traceback options

--- tests/cli/test_stderr.py
+++ tests/cli/test_stderr.py
@@ -5,6 +5,9 @@ from mlem.core.errors import MlemError
 from mlem.ui import echo, stderr_echo
 
 EXCEPTION_MESSAGE = "Test Exception Message"
+MLEM_ERROR_MESSAGE = "Test Mlem Error Message"
+STDERR_MESSAGE = "Test Stderr Message"
+TRACEBACK_IDENTIFIER_MESSAGE = "raise effect"
 
 
 def test_stderr_exception(runner):
@@ -24,9 +27,6 @@ def test_stderr_exception(runner):
         assert EXCEPTION_MESSAGE in result.stderr
 
 
-MLEM_ERROR_MESSAGE = "Test Mlem Error Message"
-
-
 def test_stderr_mlem_error(runner):
     # patch the ls command and ensure it throws a mlem error.
     with mock.patch(
@@ -44,9 +44,6 @@ def test_stderr_mlem_error(runner):
         assert MLEM_ERROR_MESSAGE in result.stderr
 
 
-STDERR_MESSAGE = "Test Stderr Message"
-
-
 def test_stderr_echo():
     with mock.patch("sys.stderr", new_callable=StringIO) as mock_stderr:
         with stderr_echo():
@@ -55,3 +52,79 @@ def test_stderr_echo():
             output = mock_stderr.read()
             assert len(output) > 0, "Output is empty, but should not be"
             assert STDERR_MESSAGE in output
+
+
+def test_traceback_option_exception(runner):
+    # patch the ls command and ensure it throws an expection.
+    with mock.patch(
+        "mlem.api.commands.ls", side_effect=Exception(EXCEPTION_MESSAGE)
+    ):
+        result = runner.invoke(
+            ["--tb", "list"],
+        )
+        assert result.exit_code == 1, (
+            result.stdout,
+            result.stderr,
+            result.exception,
+        )
+
+        assert len(result.stderr) > 0, "Output is empty, but should not be"
+        assert TRACEBACK_IDENTIFIER_MESSAGE in result.stderr
+        assert EXCEPTION_MESSAGE in result.stderr
+
+
+def test_traceback_option_mlem_error(runner):
+    # patch the ls command and ensure it throws an expection.
+    with mock.patch(
+        "mlem.api.commands.ls", side_effect=MlemError(MLEM_ERROR_MESSAGE)
+    ):
+        result = runner.invoke(
+            ["--tb", "list"],
+        )
+        assert result.exit_code == 1, (
+            result.stdout,
+            result.stderr,
+            result.exception,
+        )
+
+        assert len(result.stderr) > 0, "Output is empty, but should not be"
+        assert TRACEBACK_IDENTIFIER_MESSAGE in result.stderr
+        assert MLEM_ERROR_MESSAGE in result.stderr
+
+
+def test_verbose_option_exception(runner):
+    # patch the ls command and ensure it throws an expection.
+    with mock.patch(
+        "mlem.api.commands.ls", side_effect=Exception(EXCEPTION_MESSAGE)
+    ):
+        result = runner.invoke(
+            ["--verbose", "list"],
+        )
+        assert result.exit_code == 1, (
+            result.stdout,
+            result.stderr,
+            result.exception,
+        )
+
+        assert len(result.stderr) > 0, "Output is empty, but should not be"
+        assert TRACEBACK_IDENTIFIER_MESSAGE in result.stderr
+        assert EXCEPTION_MESSAGE in result.stderr
+
+
+def test_verbose_option_mlem_error(runner):
+    # patch the ls command and ensure it throws an expection.
+    with mock.patch(
+        "mlem.api.commands.ls", side_effect=MlemError(MLEM_ERROR_MESSAGE)
+    ):
+        result = runner.invoke(
+            ["--verbose", "list"],
+        )
+        assert result.exit_code == 1, (
+            result.stdout,
+            result.stderr,
+            result.exception,
+        )
+
+        assert len(result.stderr) > 0, "Output is empty, but should not be"
+        assert TRACEBACK_IDENTIFIER_MESSAGE in result.stderr
+        assert MLEM_ERROR_MESSAGE in result.stderr
