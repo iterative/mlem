39fcd578007d4a20f493248884b54b282e1e3b2a
Madhur Tandon <madhurtandon23@gmail.com>
2022-10-12 14:54:42 +0530

    suggested improvements

--- mlem/contrib/venv.py
+++ mlem/contrib/venv.py
@@ -5,11 +5,35 @@ import sys
 import venv
 from abc import abstractmethod
 from types import SimpleNamespace
-from typing import ClassVar, Optional
+from typing import ClassVar, List, Optional
 
 from mlem.core.errors import MlemError
 from mlem.core.objects import MlemBuilder, MlemModel
 from mlem.ui import EMOJI_OK, EMOJI_PACK, echo
+
+
+def get_python_exe_in_virtual_env(env_dir: str, env_type: str = "pip"):
+    if platform.system() == "Windows":
+        if env_type == "pip":
+            env_exe = os.path.join(env_dir, "Scripts", "python.exe")
+        elif env_type == "conda":
+            env_exe = os.path.join(env_dir, "python.exe")
+    else:
+        env_exe = os.path.join(env_dir, "bin", "python")
+    return env_exe
+
+
+def run_in_subprocess(cmd: List[str], error_msg: str, check_output=False):
+    try:
+        if check_output:
+            return subprocess.check_output(cmd)
+        return subprocess.run(cmd, check=True)
+    except (
+        FileNotFoundError,
+        subprocess.CalledProcessError,
+        subprocess.TimeoutExpired,
+    ) as e:
+        raise MlemError(f"{error_msg}\n{e}") from e
 
 
 class EnvBuilder(MlemBuilder):
@@ -25,23 +49,20 @@ class EnvBuilder(MlemBuilder):
         raise NotImplementedError
 
     @abstractmethod
-    def get_installed_packages(self, env_dir):
-        raise NotImplementedError
-
-    @abstractmethod
-    def build(self, obj: MlemModel) -> str:
+    def get_installed_packages(self, env_dir: str):
         raise NotImplementedError
 
 
 class VenvBuilder(EnvBuilder):
     class Config:
         arbitrary_types_allowed = True
+        exclude = {"context"}
 
     type: ClassVar = "venv"
 
-    no_cache: Optional[bool] = False
+    no_cache: bool = False
     """Disable cache"""
-    current_env: Optional[bool] = False
+    current_env: bool = False
     """Whether to install in the current virtual env, must be active"""
     context: Optional[SimpleNamespace] = None
     """context for the virtual env"""
@@ -51,9 +72,6 @@ class VenvBuilder(EnvBuilder):
         env_dir = os.path.abspath(self.target)
 
         self.context = env_spec.ensure_directories(env_dir)
-
-        true_system_site_packages = env_spec.system_site_packages
-        env_spec.system_site_packages = False
         env_spec.create_configuration(self.context)
         env_spec.setup_python(self.context)
         if env_spec.with_pip:
@@ -63,28 +81,14 @@ class VenvBuilder(EnvBuilder):
         if not env_spec.upgrade:
             env_spec.setup_scripts(self.context)
             env_spec.post_setup(self.context)
-        if true_system_site_packages:
-            env_spec.system_site_packages = True
-            env_spec.create_configuration(self.context)
 
     def get_installed_packages(self, env_dir):
-        if platform.system() == "Windows":
-            env_exe = os.path.join(env_dir, "Scripts", "python.exe")
-        else:
-            env_exe = os.path.join(env_dir, "bin", "python")
-
-        try:
-            return subprocess.check_output([env_exe, "-m", "pip", "freeze"])
-        except FileNotFoundError as e:
-            raise MlemError(f"Executable for pip not found.\n{e}") from e
-        except subprocess.CalledProcessError as e:
-            raise MlemError(
-                f"Couldn't get the list of packages from pip.\n{e}"
-            ) from e
-        except subprocess.TimeoutExpired as e:
-            raise MlemError(
-                f"Determining list of pip packages timed out.\n{e}"
-            ) from e
+        env_exe = get_python_exe_in_virtual_env(env_dir)
+        return run_in_subprocess(
+            [env_exe, "-m", "pip", "freeze"],
+            error_msg="Error running pip",
+            check_output=True,
+        )
 
     def build(self, obj: MlemModel):
         if self.current_env:
@@ -95,10 +99,7 @@ class VenvBuilder(EnvBuilder):
                 raise MlemError("No virtual environment detected.")
             echo(EMOJI_PACK + f"Detected the virtual env {sys.prefix}")
             env_dir = sys.prefix
-            if platform.system() == "Windows":
-                env_exe = os.path.join(env_dir, "Scripts", "python.exe")
-            else:
-                env_exe = os.path.join(env_dir, "bin", "python")
+            env_exe = get_python_exe_in_virtual_env(env_dir)
         else:
             echo(EMOJI_PACK + f"Creating virtual env {self.target}...")
             self.create_virtual_env()
@@ -112,17 +113,14 @@ class VenvBuilder(EnvBuilder):
         if self.no_cache:
             install_cmd.append("--no-cache-dir")
         install_cmd.extend(obj.requirements.to_pip())
-        try:
-            subprocess.run(install_cmd, check=True)
-        except FileNotFoundError as e:
-            raise MlemError(f"Executable for pip not found.\n{e}") from e
-        except subprocess.CalledProcessError as e:
-            raise MlemError(f"Couldn't install packages from pip.\n{e}") from e
-        except subprocess.TimeoutExpired as e:
-            raise MlemError(f"Installing pip packages timed out.\n{e}") from e
+        run_in_subprocess(install_cmd, error_msg="Error running pip")
+        if platform.system() == "Windows":
+            activate_cmd = f"`{self.target}\\Scripts\\activate`"
+        else:
+            activate_cmd = f"`source {self.target}/bin/activate`"
         echo(
             EMOJI_OK
-            + f"virtual environment `{self.target}` is ready, activate with `source {self.target}/bin/activate`"
+            + f"virtual environment `{self.target}` is ready, activate with {activate_cmd}"
         )
         return env_dir
 
@@ -138,30 +136,17 @@ class CondaBuilder(EnvBuilder):
 
     def create_virtual_env(self):
         create_cmd = ["--prefix", self.target, f"python={self.python_version}"]
-        try:
-            subprocess.run(["conda", "create", "-y", *create_cmd], check=True)
-        except FileNotFoundError as e:
-            raise MlemError(f"Executable for conda not found.\n{e}") from e
-        except subprocess.CalledProcessError as e:
-            raise MlemError(
-                f"Couldn't create the conda environment.\n{e}"
-            ) from e
-        except subprocess.TimeoutExpired as e:
-            raise MlemError(
-                f"Creating conda environment timed out.\n{e}"
-            ) from e
+        run_in_subprocess(
+            ["conda", "create", "-y", *create_cmd],
+            error_msg="Error running conda",
+        )
 
     def get_installed_packages(self, env_dir):
-        try:
-            return subprocess.check_output(
-                ["conda", "list", "--prefix", env_dir]
-            )
-        except FileNotFoundError as e:
-            raise MlemError(f"Executable for conda not found.\n{e}") from e
-        except subprocess.CalledProcessError as e:
-            raise MlemError(f"Couldn't list packages from conda.\n{e}") from e
-        except subprocess.TimeoutExpired as e:
-            raise MlemError(f"Listing conda packages timed out.\n{e}") from e
+        return run_in_subprocess(
+            ["conda", "list", "--prefix", env_dir],
+            error_msg="Error running conda",
+            check_output=True,
+        )
 
     def build(self, obj: MlemModel):  # pylint: disable=too-many-branches
         pip_based_packages = obj.requirements.to_pip()
@@ -178,51 +163,25 @@ class CondaBuilder(EnvBuilder):
             assert self.target is not None
             self.create_virtual_env()
             env_dir = self.target
-            if platform.system() == "Windows":
-                env_exe = os.path.join(env_dir, "python.exe")
-            else:
-                env_exe = os.path.join(env_dir, "bin", "python")
-        try:
-            if conda_based_packages:
-                ret = subprocess.run(
-                    [
-                        "conda",
-                        "install",
-                        "--prefix",
-                        env_dir,
-                        "-y",
-                        *conda_based_packages,
-                    ],
-                    check=True,
-                )
-                assert ret.returncode == 0
-        except FileNotFoundError as e:
-            raise MlemError(f"Executable for conda not found.\n{e}") from e
-        except subprocess.CalledProcessError as e:
-            raise MlemError(
-                f"Couldn't install packages in conda environment: {self.target}.\n{e}"
-            ) from e
-        except subprocess.TimeoutExpired as e:
-            raise MlemError(
-                f"Installing conda packages timed out.\n{e}"
-            ) from e
+            env_exe = get_python_exe_in_virtual_env(env_dir, env_type="conda")
+        if conda_based_packages:
+            run_in_subprocess(
+                [
+                    "conda",
+                    "install",
+                    "--prefix",
+                    env_dir,
+                    "-y",
+                    *conda_based_packages,
+                ],
+                error_msg="Error running conda",
+            )
 
         # install pip packages in conda env
         if pip_based_packages:
-            try:
-                subprocess.run(
-                    [env_exe, "-m", "pip", "install", *pip_based_packages],
-                    check=True,
-                )
-            except FileNotFoundError as e:
-                raise MlemError(f"Executable for pip not found.\n{e}") from e
-            except subprocess.CalledProcessError as e:
-                raise MlemError(
-                    f"Couldn't install packages in conda environment using pip: {self.target}.\n{e}"
-                ) from e
-            except subprocess.TimeoutExpired as e:
-                raise MlemError(
-                    f"Installing pip based packages in conda environment timed out.\n{e}"
-                ) from e
+            run_in_subprocess(
+                [env_exe, "-m", "pip", "install", *pip_based_packages],
+                error_msg="Error running pip",
+            )
 
         return env_dir
