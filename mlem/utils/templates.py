15e772f175e31bd70a1b9c6e53825393ad17d0df
viktor trokhymenko <trokhymenkoviktor@gmail.com>
2022-10-21 14:16:22 +0300

    fix: try for `autoescape` into `jinja2.Environment

--- mlem/utils/templates.py
+++ mlem/utils/templates.py
@@ -2,7 +2,7 @@ from typing import ClassVar, List
 
 from fsspec import AbstractFileSystem
 from fsspec.implementations.local import LocalFileSystem
-from jinja2 import Environment, FileSystemLoader, StrictUndefined
+from jinja2 import Environment, FileSystemLoader, StrictUndefined, select_autoescape
 from pydantic import BaseModel
 
 
@@ -22,7 +22,7 @@ class TemplateModel(BaseModel):
         j2 = Environment(
             loader=FileSystemLoader(self.templates_dir + [self.TEMPLATE_DIR]),
             undefined=StrictUndefined,
-            autoescape=True,
+            autoescape=select_autoescape(),
         )
         template = j2.get_template(self.TEMPLATE_FILE)
         args = self.prepare_dict()
