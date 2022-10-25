d1f653aa18c826e63778499374da82d07fb9991e
Madhur Tandon <20173739+madhur-tandon@users.noreply.github.com>
2022-10-25 16:06:56 +0530

    add requirements builder (#434)
    * add requirements builder
    
    * add builder for virtualenv
    
    * fix linting
    
    * fix pylint again
    
    * fix entrypoints
    
    * remove upgrade deps
    
    * add ability to install in current active venv
    
    * fix pylint
    
    * add ability to create conda based envs
    
    * fix import
    
    * fix has_conda check
    
    * fix windows issues
    
    * relax array comparison test
    
    * fix typo
    
    * use conda in github actions
    
    * suggested improvements
    
    * fix python version determination
    
    * make create_virtual_env consistent
    
    * suggested improvements
    
    * add test for unix based req
    
    * pass sample data
    
    * fix test
    
    * minor improvements
    
    * use load_impl_ext for requirements builder
    
    * fix tests
    
    * add test for invalid req_type
    
    * register conda mark in pytest
    
    * add test for current and active venv
    
    * fix tests
    
    * fix test
    
    * add conda reqs as list for the builder
    
    * remove usage of context and protected access
    
    * fix entrypoints
    
    * remove pylint disable
    
    * move CondaPackageRequirement to mlem.contrib.venv
    
    * use materialize
    
    * fix tests
    
    * fix docstrings
    
    * fix tests
    
    * fix docs based test
    
    * suggested changes

--- tests/contrib/conftest.py
+++ tests/contrib/conftest.py
@@ -1,9 +1,26 @@
+import subprocess
+
 import pytest
 
 from mlem.contrib.docker.context import use_mlem_source
+from tests.conftest import long
 
 
 @pytest.fixture()
 def uses_docker_build():
     with use_mlem_source("whl"):
         yield
+
+
+def has_conda():
+    try:
+        ret = subprocess.run(["conda"], check=True)
+        return ret.returncode == 0
+    except FileNotFoundError:
+        return False
+
+
+def conda_test(f):
+    mark = pytest.mark.conda
+    skip = pytest.mark.skipif(not has_conda(), reason="conda is unavailable")
+    return long(mark(skip(f)))
