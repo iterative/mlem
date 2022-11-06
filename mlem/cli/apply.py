b3cebf3f391f14de387c839735fa45f9b9b6c133
omesser <odedmesser@gmail.com>
2022-11-06 20:25:13 +0200

    on remote and deploy apply as well

--- mlem/cli/apply.py
+++ mlem/cli/apply.py
@@ -166,7 +166,7 @@ def _apply_remote(
             output,
             target_project,
         )
-    if output is None and json:
+    if output is None:
         print(
             dumps(
                 DataAnalyzer.analyze(result).get_serializer().serialize(result)
