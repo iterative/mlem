b39e0976be8895218f084d4f4f72e71fe82b18d6
mike0sv <mike0sv@gmail.com>
2023-01-26 03:45:06 +0300

    Add --template option for streamlit to provide easy app manipulation

--- mlem/contrib/streamlit/server.py
+++ mlem/contrib/streamlit/server.py
@@ -1,26 +1,29 @@
 import contextlib
 import os
+import shutil
 import subprocess
 import tempfile
 from time import sleep
-from typing import ClassVar
+from typing import ClassVar, Dict, Optional
 
 import streamlit
 import streamlit_pydantic
 
+from mlem.core.errors import MlemError
 from mlem.core.requirements import LibRequirementsMixin, Requirements
 from mlem.runtime import Interface
 from mlem.runtime.server import Server
+from mlem.utils.path import make_posix
 from mlem.utils.templates import TemplateModel
 
+TEMPLATE_PY = "_template.py"
 SCRIPT_PY = "script.py"
 
 
 class StreamlitScript(TemplateModel):
-    TEMPLATE_FILE: ClassVar = "_template.py"
+    TEMPLATE_FILE: ClassVar = TEMPLATE_PY
     TEMPLATE_DIR: ClassVar = os.path.dirname(__file__)
 
-    method_name: str
     server_host: str = "0.0.0.0"
     server_port: str = "8080"
 
@@ -43,9 +46,10 @@ class StreamlitServer(Server, LibRequirementsMixin):
     """Port for running Streamlit UI"""
     use_watchdog: bool = True
     """Install watchdog for better performance"""
+    template: Optional[str] = None
 
     def serve(self, interface: Interface):
-        with self.prepare_streamlit_script(interface):
+        with self.prepare_streamlit_script():
             if self.run_server:
                 from mlem.contrib.fastapi import FastAPIServer
 
@@ -57,15 +61,19 @@ class StreamlitServer(Server, LibRequirementsMixin):
                     sleep(100)
 
     @contextlib.contextmanager
-    def prepare_streamlit_script(self, interface: Interface):
+    def prepare_streamlit_script(self):
         with tempfile.TemporaryDirectory(
             prefix="mlem_streamlit_script_"
         ) as tempdir:
             path = os.path.join(tempdir, SCRIPT_PY)
+            templates_dir = []
+            if self.template is not None:
+                shutil.copy(self.template, os.path.join(tempdir, TEMPLATE_PY))
+                templates_dir = [tempdir]
             StreamlitScript(
                 server_host=self.server_host,
                 server_port=str(self.server_port),
-                method_name=interface.get_method_names()[0],
+                templates_dir=templates_dir,
             ).write(path)
             with self.run_streamlit_daemon(path):
                 yield
@@ -97,3 +105,21 @@ class StreamlitServer(Server, LibRequirementsMixin):
         if self.use_watchdog:
             reqs += Requirements.new("watchdog")
         return reqs
+
+    def get_sources(self) -> Dict[str, bytes]:
+        sources = super().get_sources()
+        if self.template is not None:
+            if not os.path.abspath(self.template).startswith(
+                os.path.abspath(".")
+            ):
+                # TODO: issue#...
+                raise MlemError(
+                    f"Template file {self.template} is not in a subtree of cwd"
+                )
+            template = make_posix(
+                os.path.relpath(os.path.abspath(self.template), ".")
+            )
+            with open(template, "rb") as f:
+                sources[template] = f.read()
+            self.template = template
+        return sources
