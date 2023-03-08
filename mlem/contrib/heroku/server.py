6c716e544c66ecc116f5b01ac83b0d600b0e8302
mike0sv <mike0sv@gmail.com>
2023-03-08 14:53:55 +0300

    Add server field to heroku deploy closes #568

--- mlem/contrib/heroku/server.py
+++ mlem/contrib/heroku/server.py
@@ -2,18 +2,34 @@ import logging
 import os
 from typing import ClassVar
 
-from mlem.contrib.fastapi import FastAPIServer
+from pydantic import validator
+
 from mlem.runtime import Interface
+from mlem.runtime.server import Server
 
 logger = logging.getLogger(__name__)
 
 
-class HerokuServer(FastAPIServer):
+class HerokuServer(Server):
     """Special FastAPI server to pickup port from env PORT"""
 
     type: ClassVar = "_heroku"
+    server: Server
+
+    @validator("server")
+    @classmethod
+    def server_validator(cls, value: Server):
+        if value.port_field is None:
+            raise ValueError(
+                f"{value} does not have port field and can not be exposed on heroku"
+            )
+        return value
 
     def serve(self, interface: Interface):
-        self.port = int(os.environ["PORT"])
-        logger.info("Switching port to %s", self.port)
-        return super().serve(interface)
+        assert self.server.port_field is not None  # ensured by validator
+        setattr(self.server, self.server.port_field, int(os.environ["PORT"]))
+        logger.info(
+            "Switching port to %s",
+            getattr(self.server, self.server.port_field),
+        )
+        return self.server.serve(interface)
